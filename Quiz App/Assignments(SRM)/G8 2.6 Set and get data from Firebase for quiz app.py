0bb20ba9871fe1eb3f59b5881c900fdcca657e90
Avani <avani.abhijit@orchids.edu.in>
2024-08-21 17:01:42 +0530

    Update G8 2.6 Set and get data from Firebase for quiz app.py

--- Quiz App/Assignments(SRM)/G8 2.6 Set and get data from Firebase for quiz app.py
+++ Quiz App/Assignments(SRM)/G8 2.6 Set and get data from Firebase for quiz app.py
@@ -11,18 +11,16 @@ firebase_admin.initialize_app(cred,
                               {'databaseURL': 'https://quiz-6a3fe-default-rtdb.asia-southeast1.firebasedatabase.app/User'})
 
 # Function to add data to Firebase
-def add_data(name, email):
-    ref = db.reference('/users')  # Reference to the 'users' node in your Firebase Realtime Database
-    new_user_ref = ref.push()  # Create a new child node under 'users'
-    new_user_ref.set({
-        'name': name,
-        'email': email
-    })
+# Function to add data to Firebase
+def add_data(name, pwd):
+    ref = db.reference('users/'+name)  # Reference to the 'users' node in your Firebase Realtime Database
+    ref.set(pwd)
 
 # Function to fetch data from Firebase
 def fetch_data():
-    ref = db.reference('/users')  # Reference to the 'users' node in your Firebase Realtime Database
+    ref = db.reference('users/')  # Reference to the 'users' node in your Firebase Realtime Database
     data = ref.get()
+    print(data)
     
 
 # Add data to Firebase
